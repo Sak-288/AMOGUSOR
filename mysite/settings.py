2f7c535f7d0b6797404d98f6e6812617b84b2167
Your Name <aminesakoute288@gmail.com>
2025-12-01 22:37:33 +0100

    DEBUG = False when in production

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'amogusor-production.up.railway.app'] # You'll need a new railway projects + link
 
